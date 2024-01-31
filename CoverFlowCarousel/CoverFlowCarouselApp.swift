cd59c18d405d3114bfbb17ebc5ae35324fbcb2e2
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-01-30 20:50:45 -0500

    Initial Commit

---
+++ CoverFlowCarousel/CoverFlowCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverFlowCarouselApp.swift
+//  CoverFlowCarousel
+//
+//  Created by Leonard McCook-Carr on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct CoverFlowCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
